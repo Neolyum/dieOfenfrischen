0e09f94ea22a2f701e52042cb07a2d48236a5915
Toni <antonia.holsten@hotmail.de>
2020-05-09 20:47:57 +0200

    added beat markers

--- Assets/Scripts/AmplitudeAnalyzer.cs
+++ Assets/Scripts/AmplitudeAnalyzer.cs
@@ -1,18 +1,88 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AmplitudeAnalyzer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioSource audioSource;
+    public AudioSource audioSourcePreload;
+    public float updateStep = 0.1f;
+    public int sampleDataLength = 1024;
 
-    // Update is called once per frame
-    void Update()
-    {
+    public GameObject BeatMarkers;
+    public GameObject beatMarkerPrefab;
+    public float delay = 2f;
+
+    private float currentUpdateTime = 0f;
+ 
+    private float clipLoudness;
+    private float[] clipSampleData;
+
+    private float highest;
+    private float lowest;
+    private float sum;
+    private float average;
+    private float counter;
+    private float offset = 0.002f;
+    private bool beatHigh = false;
         
+    // Use this for initialization
+    void Awake () {
+        
+        audioSourcePreload.Play();
+        audioSource.Play();
+
+        highest = float.MinValue;
+        lowest = float.MaxValue;
+        average = 0;
+        sum = 0;
+        
+        if (!audioSource) {
+            Debug.LogError(GetType() + ".Awake: there was no audioSource set.");
+        }
+        else {
+            Debug.Log("Awake: audioSource active");
+        }
+        clipSampleData = new float[sampleDataLength];
+ 
+    }
+     
+    // Update is called once per frame
+    void Update () {
+     
+        currentUpdateTime += Time.deltaTime;
+        if (currentUpdateTime >= updateStep) {
+            currentUpdateTime = 0f;
+            audioSourcePreload.clip.GetData(clipSampleData, audioSource.timeSamples); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
+            clipLoudness = 0f;
+            foreach (var sample in clipSampleData) {
+                clipLoudness += Mathf.Abs(sample);
+            }
+            clipLoudness /= sampleDataLength; //clipLoudness is what you are looking for
+            counter += 1;
+            highest = highest < clipLoudness ? clipLoudness : highest;
+            lowest = lowest > clipLoudness ? clipLoudness : lowest;
+            sum += clipLoudness;
+            average = sum / counter;
+            
+            Debug.Log("Highest: " + highest);
+            Debug.Log("Lowest: " + lowest);
+            Debug.Log("Average: " + average);
+
+            Debug.Log("Clip loudness: " + clipLoudness);
+
+            if (clipLoudness >= average + offset && beatHigh == false) {
+
+                GameObject beatMarker = Instantiate(beatMarkerPrefab);
+                beatMarker.transform.SetParent(BeatMarkers.transform);
+                beatHigh = true;
+            }
+            else {
+                beatHigh = false;
+            }
+        }
+ 
     }
 }
---
+++ Assets/Scripts/BeatMarkerComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatMarkerComponent : MonoBehaviour {
+    
+    public float destroyDelay = 3f;
+    public Canvas UI;
+    
+
+    private void Start() {
+        transform.position = new Vector3(transform.position.x + 200, transform.position.y + 75, transform.position.z);
+        Destroy(gameObject, destroyDelay);
+    }
+
+    private void FixedUpdate() {
+        gameObject.transform.position = new Vector3(transform.position.x + 4.0f, transform.position.y, transform.position.z);
+    }
+}
